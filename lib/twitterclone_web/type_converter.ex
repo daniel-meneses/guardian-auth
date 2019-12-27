007094e43a71348dd04f8df51ab2d9a7b1c6575f
daniel-meneses <dmenesesmailbox@gmail.com>
2019-12-27 18:54:40 +1100

    Unless returns nil, using if else instead

--- lib/twitterclone_web/type_converter.ex
+++ lib/twitterclone_web/type_converter.ex
@@ -1,11 +1,20 @@
 defmodule TwittercloneWeb.TypeConverter do
 
-  def maplist_to_map(maplist) do
+  def maplist_to_map2(maplist) do
     unless maplist == [], do:
     Enum.reduce(maplist, fn x, y ->
                   Map.merge(x, y, fn _k, v1, v2 -> v2 ++ v1 end)
                end)
-    %{}
+  end
+
+  def maplist_to_map(maplist) do
+    if maplist !== [] do
+      Enum.reduce(maplist, fn x, y ->
+                    Map.merge(x, y, fn _k, v1, v2 -> v2 ++ v1 end)
+                 end)
+               else
+                 %{}
+          end
   end
 
 
