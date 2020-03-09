5f904e115e4f56b4c40c356568a6aa09aeebc627
daniel-meneses <dmenesesmailbox@gmail.com>
2020-03-09 20:22:48 +1100

    Try again

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :twitterclone, TwittercloneWeb.Endpoint,
 
 config :twitterclone, Twitterclone.Repo,
   adapter: Ecto.Adapters.Postgres,
-  #url: System.get_env("DATABASE_URL"),
+  url: System.get_env("DATABASE_URL"),
   #pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
