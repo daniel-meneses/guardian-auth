6c92b1db85fc200fbdd34a00aa6f0493c2973e25
daniel-meneses <dmenesesmailbox@gmail.com>
2020-03-09 20:20:28 +1100

    Try updating repo config

--- config/prod.exs
+++ config/prod.exs
@@ -17,8 +17,8 @@ config :twitterclone, TwittercloneWeb.Endpoint,
 
 config :twitterclone, Twitterclone.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  #url: System.get_env("DATABASE_URL"),
+  #pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
 # Do not print debug messages in production
